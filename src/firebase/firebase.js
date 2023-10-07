f5fd00b0af27104bc64d15164dd8b5799e3b15bd
aallawi <allawi100@yahoo.com>
2023-10-07 16:34:25 +0300

    add .env.local file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhNl0ge5LYXyYgoRo0E_dDn2Uc1HIrJtA",
-  authDomain: "my-wallet-d1ce7.firebaseapp.com",
-  projectId: "my-wallet-d1ce7",
-  storageBucket: "my-wallet-d1ce7.appspot.com",
-  messagingSenderId: "562869501280",
-  appId: "1:562869501280:web:9c1c4a43a44c37ca1db205",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
